cbe617b7e872e3691c4403d66693bff71ddfd853
futurekang <582727108@qq.com>
2019-12-03 11:40:28 +0800

    1

--- app/src/main/java/com/futurekang/example/MainActivity.java
+++ app/src/main/java/com/futurekang/example/MainActivity.java
@@ -1,21 +1,45 @@
 package com.futurekang.example;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.futurekang.pictureselector.activity.AlbumPreviewActivity;
+import com.futurekang.pictureselector.model.MediaFileInfo;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.btn_go);
-        button.setOnClickListener(v -> AlbumPreviewActivity.toAlbumPreview(MainActivity.this, 9));
+        button.setOnClickListener(v ->
+                AlbumPreviewActivity.toAlbumPreview(MainActivity.this, 9)
+        );
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            assert data != null;
+            ArrayList<MediaFileInfo> mediaFileInfos = data.getParcelableArrayListExtra("media_info");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                mediaFileInfos.forEach(mediaFileInfo -> Log.d(TAG, "accept: " + mediaFileInfo.getFilePath()));
+            }
+        }
+    }
+
 }
